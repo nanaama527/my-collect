8424696aabfe108af44cb387c6eea0d9413465d3
nanaama527 <72888369+nanaama527@users.noreply.github.com>
2020-11-02 06:52:31 -0500

    Update my_collect.rb

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(array)
+    n = 0
+    collection = 0
+    while n < array.length 
+        yield(array[n])
+        n += 1      
+    end
+    collection
+end
 
-
+def my_collect(students)
+    n = 0
+    students = ["Tim", "Tom", "Sophie", "Antoin"]
+    my_collect(students) do |name|
+        students.split(" ").first
+    end
+    collection
+end
