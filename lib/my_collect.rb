7ba1deda312f16360f2c9ba0f8d9171eed4c7c8d
Nana <72888369+nanaama527@users.noreply.github.com>
2021-01-14 12:17:50 -0500

    Update my_collect.rb

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+# require 'pry'
 
+
+def my_collect(array)
+    i = 0
+    new_array = []
+    while i < array.length
+        new_array << yield(array[i])
+        i += 1
+    end 
+    new_array
+end
 
